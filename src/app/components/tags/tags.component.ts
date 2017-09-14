189e0f7ac31b22b52840409a9588944d25d9292d
Yuri Korkishko <yorikworld@gmail.com>
2017-09-14 15:33:13 +0300

    #3 fixed footer align, fixed about us page

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -4,7 +4,7 @@ import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-tags',
-  template: `<a [routerLink]="['tag', tag.slug]" *ngFor="let tag of postTags" class="category-tag" [ngStyle]="{'background': tag.color}" rel="category tag">{{tag.name}}</a>&nbsp;`,
+  template: `<a [routerLink]="['tag', tag.slug]" *ngFor="let tag of postTags" class="category-tag" [ngStyle]="{'background': tag.color}" rel="category tag">#{{tag.name}}</a>&nbsp;`,
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent implements OnInit, OnDestroy {
